7ed0a8115d55c6437b1640896a969ae15a4b352f
NotSnowWhite <gflatch@att.net>
2024-04-20 17:05:07 -0500

    added fs write file function but i dont really get it....and added require()

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile('README.md', data, (err) =>
+        err ? console.error(err) : console.log('Successful README file created!')
+    )
+}
 
 // TODO: Create a function to initialize app
 function init() {}
